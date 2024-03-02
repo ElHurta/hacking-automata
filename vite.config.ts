0b6302184f0b64b6a137649e42aa566c5fdff39a
ElHurta <juan.david.hurtado08@gmail.com>
2024-03-02 12:21:03 -0500

    Fixing Havok file creation error

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    fs: {
+      // Allow serving files outside of the root
+      allow: [
+        "../.."
+      ]
+    }
+  },
+  optimizeDeps: { exclude: ["@babylonjs/havok"] },
 });
