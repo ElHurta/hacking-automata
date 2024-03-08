4c42bde2f3f4e84e73f82ab2091973ab796837f3
ElHurta <juan.david.hurtado08@gmail.com>
2024-03-08 17:44:53 -0500

    Fixing enemy teleporting when colliding with objects

--- src/utils/setRenderer.ts
+++ src/utils/setRenderer.ts
@@ -4,18 +4,26 @@ export function syncPosition(
   entity: YUKA.GameEntity,
   mesh: AbstractMesh,
 ): void {
+  mesh.rotationQuaternion = new Quaternion(
+    0,
+    entity.rotation.y,
+    0,
+    entity.rotation.w,
+  );
+  
+  if (mesh.collider?.collidedMesh){
+    entity.position.set(
+      mesh.position.x,
+      mesh.position.y,
+      mesh.position.z,
+    );
+  }
+
   mesh.moveWithCollisions(
     new Vector3(
       entity.position.x - mesh.position.x,
       0,
       entity.position.z - mesh.position.z,
     ),
-  );
-
-  mesh.rotationQuaternion = new Quaternion(
-    0,
-    entity.rotation.y,
-    0,
-    entity.rotation.w,
   );
 }
