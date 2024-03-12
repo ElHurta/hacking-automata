644c124c0bbc655ff0a1ba906c5df97c061a750f
ElHurta <juan.david.hurtado08@gmail.com>
2024-03-12 16:08:28 -0500

    Preparing Life Points featuree

--- src/babylonManager/Entities/Enemy.ts
+++ src/babylonManager/Entities/Enemy.ts
@@ -1,11 +1,9 @@
 import { AbstractMesh } from "@babylonjs/core";
 import * as YUKA from "yuka";
 
-export default class Player {
-  private _MOVEMENT_SPEED = 0.5;
+export default class Enemy {
   private _mesh!: AbstractMesh;
   private _movingEntity = new YUKA.MovingEntity();
-  private _lifePoints = 3;
 
   constructor() {}
 
@@ -23,13 +21,5 @@ export default class Player {
 
   public set movingEntity(entity: YUKA.MovingEntity) {
     this._movingEntity = entity;
-  }
-
-  public get MOVEMENT_SPEED(): number {
-    return this._MOVEMENT_SPEED;
-  }
-
-  public get lifePoints(): number {
-    return this._lifePoints;
   }
 }
